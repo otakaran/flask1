7dcac16f02310c508d6081cb235108850707dfd6
Otakar Andrysek <otakar@me.com>
2021-01-11 15:17:47 -0800

    cleaned up code

--- sample_backend.py
+++ sample_backend.py
@@ -1,37 +1,35 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 users = { 
-   'users_list' :
-   [
-      { 
-         'id' : 'xyz789',
-         'name' : 'Charlie',
-         'job': 'Janitor',
-      },
-      {
-         'id' : 'abc123', 
-         'name': 'Mac',
-         'job': 'Bouncer',
-      },
-      {
-         'id' : 'ppp222', 
-         'name': 'Mac',
-         'job': 'Professor',
-      }, 
-      {
-         'id' : 'yat999', 
-         'name': 'Dee',
-         'job': 'Aspring actress',
-      },
-      {
-         'id' : 'zap555', 
-         'name': 'Dennis',
-         'job': 'Bartender',
-      }
-   ]
+    'users_list' :
+    [
+        { 
+            'id' : 'xyz789',
+            'name' : 'Charlie',
+            'job': 'Janitor',
+        },
+        {
+            'id' : 'abc123', 
+            'name': 'Mac',
+            'job': 'Bouncer',
+        },
+        {
+            'id' : 'ppp222', 
+            'name': 'Mac',
+            'job': 'Professor',
+        }, 
+        {
+            'id' : 'yat999', 
+            'name': 'Dee',
+            'job': 'Aspring actress',
+        },
+        {
+            'id' : 'zap555', 
+            'name': 'Dennis',
+            'job': 'Bartender',
+        }
+    ]
 }
 
 @app.route('/')
@@ -40,28 +38,28 @@ def hello_world():
 
 @app.route('/users/<id>')
 def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
+    if id :
+        for user in users['users_list']:
+            if user['id'] == id:
+                return user
+        return ({})
+    return users
 
 @app.route('/users', methods=['GET', 'POST'])
 def get_users():
-   if request.method == 'GET':
-      search_username = request.args.get('name')
-      if search_username :
-         subdict = {'users_list' : []}
-         for user in users['users_list']:
-            if user['name'] == search_username:
-               subdict['users_list'].append(user)
-         return subdict
-      return users
-   elif request.method == 'POST':
-      userToAdd = request.get_json()
-      users['users_list'].append(userToAdd)
-      resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
-      # 200 is the default code for a normal response
-      return resp
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        if search_username:
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['name'] == search_username:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success = True)
+        #resp.status_code = 200 #optionally, you can always set a response code. 
+        # 200 is the default code for a normal response
+        return resp
